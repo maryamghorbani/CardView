5e57e57425b7bd7917a32153f90a9018a7007de6
Maria Ghorbani <ghorbanimoghaddam.m@gmail.com>
2022-01-09 23:48:48 +0200

    import data from api

--- src/features/pokemon/BackSide.tsx
+++ src/features/pokemon/BackSide.tsx
@@ -1,20 +1,26 @@
 import ProgressBar from "@ramonak/react-progress-bar";
+import {Pokemon} from "../pokemonDetails/pokemonAPI";
+import {useAppSelector} from "../../app/hooks";
+import {selectPokemon} from "../pokemonDetails/pokemonSlice";
 
 export function BackSide() {
+
+    const pokemon: Pokemon|null = useAppSelector(selectPokemon);
+
     return (
         <div className="w-card h-card p-24 bg-white shadow-md rounded-lg text-gray-900">
             <div className="text-center mt-2 mb-28">
-                <p className="text-stone-300 text-4xl font-extrabold">#1</p>
-                <p className="text-7xl font-black">Blubasaur</p>
+                <p className="text-stone-300 text-4xl font-extrabold">#{pokemon?.id}</p>
+                <p className="text-7xl font-black">{pokemon?.name}</p>
             </div>
 
             <div className="">
                 <div className="relative mb-9">
-                    <p className="text-left text-2xl font-extrabold text-red-600 pl-4">HP</p>
+                    <p className="text-left text-2xl font-extrabold text-red-600 pl-7">HP</p>
                     <ProgressBar className="justify-center flex items-center" completed="45" maxCompleted={255} bgColor="#ff4545" width="80%" height="10px" baseBgColor="#f5f5f5" labelAlignment="outside" labelColor="#ff4545" labelSize="1.5625rem" />
                 </div>
                 <div className="relative mb-9">
-                    <p className="text-left text-2xl font-extrabold text-red-600 pl-4">HP</p>
+                    <p className="text-left text-2xl font-extrabold text-red-600 pl-7">HP</p>
                     <ProgressBar className="justify-center flex items-center" completed="45" maxCompleted={255} bgColor="#ff4545" width="80%" height="10px" baseBgColor="#f5f5f5" labelAlignment="outside" labelColor="#ff4545" labelSize="1.5625rem" />
                 </div>
             </div>
--- src/features/pokemon/FrontSide.tsx
+++ src/features/pokemon/FrontSide.tsx
@@ -1,14 +1,21 @@
+import {Pokemon} from "../pokemonDetails/pokemonAPI";
+import {useAppSelector} from "../../app/hooks";
+import {selectPokemon} from "../pokemonDetails/pokemonSlice";
+
+
 export function FrontSide() {
+
+    const pokemon: Pokemon|null = useAppSelector(selectPokemon);
+
     return (
-        <div className="w-card h-card p-24 bg-white shadow-md rounded-lg text-gray-900">
+        <div className="translate-y-full w-card h-card p-24 bg-white shadow-md rounded-lg text-gray-900">
             <div className="text-center mt-2">
-                <p className="text-stone-300 text-4xl font-extrabold">#1</p>
-                <p className="text-7xl font-black">Blubasaur</p>
+                <p className="text-stone-300 text-4xl font-extrabold">#{pokemon?.id}</p>
+                <p className="text-7xl font-black">{pokemon?.name}</p>
             </div>
             <div className="justify-center flex">
                 <img className=""
-                     src='https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ'
-                     alt='Woman looking front'/>
+                     src={pokemon?.image}/>
             </div>
             <div className="px-5 py-2.5 mt-20 mb-5">
                 <button className="inline-block rounded-lg bg-gray-400 font-semibold text-white px-6 py-1 text-lg">Follow
