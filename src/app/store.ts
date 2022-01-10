056352edb7c674f63c0bc62872b98a50f27f0662
Maria Ghorbani <ghorbanimoghaddam.m@gmail.com>
2022-01-10 22:18:15 +0200

    Install and configure eslint

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,9 +2,9 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import pokemonReducer from '../features/pokemonDetails/pokemonSlice';
 
 export const store = configureStore({
-	reducer: {
-		pokemon: pokemonReducer,
-	},
+  reducer: {
+    pokemon: pokemonReducer,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
